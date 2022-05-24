d46518fd2d3d0840cdf5340d333f618c446b6e53
Hatice GAZEL <98157466+gazellhatice@users.noreply.github.com>
2022-05-24 19:53:54 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+//Üç kenar uzunluğunu kullanıcıdan aldığınız üçgenin alanını hesaplayan programı yazınız.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int a, b;
+        double c, u, alan;
+
+        System.out.print("a kenarının değerini giriniz: ");
+        a = inp.nextInt();
+
+        System.out.print("b kenarının değerini giriniz: ");
+        b= inp.nextInt();
+
+        c = Math.sqrt((a * a) + (b * b));
+        System.out.println("Hipotenüs uzunluğu:" + c);
+
+        u = (a + b + c) / 2;
+        System.out.println("Üçgenin çevresi:" + 2 * u);
+
+        alan = Math.sqrt(u * (u - a) * (u - b) * (u - c));
+        System.out.print("Üçgenin alanı:" + (alan * alan));
+    }
+}
